82afbd9c293a943c32b41a205a52b1d495d512f3
thelemon2020 <thelemon.chris@gmail.com>
2026-03-17 20:35:03 -0400

    don't encrypt ab_variant cookie

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: ['/mirror-log']);
+        $middleware->encryptCookies(except: ['ab_variant']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
