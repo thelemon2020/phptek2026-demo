e2206427ece198962cac8199f43a0583fbe13daf
thelemon2020 <thelemon.chris@gmail.com>
2026-04-06 09:36:44 -0400

    add type

--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class PrivateController extends Controller
 {
-    public function index($request): ResponseFactory|Response|JsonResponse
+    public function index(Request $request): ResponseFactory|Response|JsonResponse
     {
         $token = $request->header('X-Auth-Token');
         if ($token === config('demo.auth_token')){
