e9889236241b466316ff8ad28e67b2ee8829bcfa
thelemon2020 <thelemon.chris@gmail.com>
2026-04-06 09:14:10 -0400

    fix issue with auth example

--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class PrivateController extends Controller
 {
-    public function index(): JsonResponse
+    public function index($request): ResponseFactory|Response|JsonResponse
     {
-        return response()->json([
-            'access' => 'granted',
-            'message' => 'You are authenticated',
-        ]);
+        $token = $request->header('X-Auth-Token');
+        if ($token === config('demo.auth_token')){
+            return response()->json([
+                'access' => 'granted',
+                'message' => 'You are authenticated',
+            ]);
+        }
+        return response('', 401);
     }
 }
